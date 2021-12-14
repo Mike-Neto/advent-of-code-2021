568de416f700100aaa7d9e5ee4ee023badb2f8ff
Miguel Mendes <miguelmendeseiriz@gmail.com>
2021-12-14 22:44:42 +0000

    day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,11 @@ fn main() {
         "The power consumption of the submarine is: {}",
         day_3_part_1("./data/day_3_data.txt").unwrap()
     );
+
+    println!(
+        "The power consumption of the submarine is: {}",
+        day_3_part_2("./data/day_3_data.txt").unwrap()
+    );
 }
 
 fn day_3_part_1(path: &str) -> Result<u64, std::io::Error> {
@@ -79,6 +84,89 @@ fn day_3_part_1(path: &str) -> Result<u64, std::io::Error> {
     }
 
     Ok(gamma_value * epsilon_value)
+}
+
+fn day_3_part_2(path: &str) -> Result<u64, std::io::Error> {
+    let file = std::fs::read_to_string(path)?;
+    let values: Vec<Vec<&str>> = file
+        .split('\n')
+        .filter_map(|s| {
+            if s.is_empty() {
+                None
+            } else {
+                Some(s.split("").filter(|s| !s.is_empty()).collect())
+            }
+        })
+        .collect();
+
+    let mut current_bit = 0usize;
+    let mut current_values: Vec<&Vec<&str>> = values.iter().collect();
+    while current_values.len() != 1 && !current_values.is_empty() {
+        current_values = filter_values(current_values, current_bit, true);
+        current_bit += 1;
+    }
+    let oxygen = bits_to_u64(current_values[0]);
+
+    let mut current_bit = 0usize;
+    let mut current_values: Vec<&Vec<&str>> = values.iter().collect();
+    while current_values.len() != 1 && !current_values.is_empty() {
+        current_values = filter_values(current_values, current_bit, false);
+        current_bit += 1;
+    }
+    let co2 = bits_to_u64(current_values[0]);
+
+    Ok(oxygen * co2)
+}
+
+fn bits_to_u64(chars: &[&str]) -> u64 {
+    let mut result = 0u64;
+    for (index, &bit) in chars.iter().enumerate() {
+        if bit == "1" {
+            let shift = (chars.len() - 1) - index;
+            let value: u64 = 1 << shift;
+            result |= value;
+        }
+    }
+    result
+}
+
+fn filter_values<'a>(
+    current_values: Vec<&'a Vec<&str>>,
+    current_bit: usize,
+    is_oxygen_generator_rating: bool,
+) -> Vec<&'a Vec<&'a str>> {
+    let (ones, zeros) =
+        current_values
+            .iter()
+            .map(|v| v[current_bit])
+            .fold((0, 0), |(mut ones, mut zeros), v| {
+                if v == "1" {
+                    ones += 1;
+                } else {
+                    zeros += 1;
+                }
+
+                (ones, zeros)
+            });
+
+    let filtered_values: Vec<&Vec<&str>> = current_values
+        .into_iter()
+        .filter(|v| {
+            if is_oxygen_generator_rating {
+                if ones >= zeros {
+                    v[current_bit] == "1"
+                } else {
+                    v[current_bit] == "0"
+                }
+            } else if zeros > ones {
+                v[current_bit] == "1"
+            } else {
+                v[current_bit] == "0"
+            }
+        })
+        .collect();
+
+    filtered_values
 }
 
 #[derive(Debug)]
@@ -195,7 +283,8 @@ fn day_one_part_two(path: &str) -> Result<u64, std::io::Error> {
 #[cfg(test)]
 mod tests {
     use crate::{
-        day_3_part_1, day_one_part_one, day_one_part_two, day_two_part_one, day_two_part_two,
+        day_3_part_1, day_3_part_2, day_one_part_one, day_one_part_two, day_two_part_one,
+        day_two_part_two,
     };
 
     #[test]
@@ -226,5 +315,11 @@ mod tests {
     fn day_3_part_1_example() {
         let result = day_3_part_1("./data/day_3_example.txt").unwrap();
         assert_eq!(result, 198);
+    }
+
+    #[test]
+    fn day_3_part_2_example() {
+        let result = day_3_part_2("./data/day_3_example.txt").unwrap();
+        assert_eq!(result, 230);
     }
 }
