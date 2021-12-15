b9c838cabb3a4968be78b971326e02ab68186326
Miguel Mendes <miguelmendeseiriz@gmail.com>
2021-12-15 20:01:17 +0000

    day 4 part 2.

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,54 @@ fn main() {
         "Your final score will be: {}",
         day_4_part_1("./data/day_4_data.txt").unwrap()
     );
+
+    println!(
+        "The last winning board final score would be: {}",
+        day_4_part_2("./data/day_4_data.txt").unwrap()
+    );
+}
+
+fn day_4_part_2(path: &str) -> Result<u64, std::io::Error> {
+    let file = std::fs::read_to_string(path)?;
+    let values: Vec<&str> = file.split('\n').filter(|s| !s.is_empty()).collect();
+    let draw_numbers: Vec<u32> = values[0]
+        .split(',')
+        .filter_map(|v| v.parse().ok())
+        .collect();
+
+    let boards: Vec<Vec<Vec<u32>>> = values
+        .iter()
+        .skip(1)
+        .chunks()
+        .map(|[a, b, c, d, e]| {
+            vec![
+                a.split(' ').filter_map(|v| v.parse().ok()).collect(),
+                b.split(' ').filter_map(|v| v.parse().ok()).collect(),
+                c.split(' ').filter_map(|v| v.parse().ok()).collect(),
+                d.split(' ').filter_map(|v| v.parse().ok()).collect(),
+                e.split(' ').filter_map(|v| v.parse().ok()).collect(),
+            ]
+        })
+        .collect();
+
+    let mut incomplete_boards: Vec<&Vec<Vec<u32>>> = boards.iter().collect();
+    for i in 0..(draw_numbers.len() - 1) {
+        let numbers = &draw_numbers[0..i];
+        let mut removed = 0usize;
+        for (index, board) in incomplete_boards.clone().iter().enumerate() {
+            let bingo = calculate_bingo(numbers, board);
+            if let Some(score) = bingo {
+                if incomplete_boards.len() == 1 {
+                    return Ok(score as u64);
+                } else {
+                    incomplete_boards.remove(index - removed);
+                    removed += 1;
+                }
+            }
+        }
+    }
+
+    Ok(0)
 }
 
 fn day_4_part_1(path: &str) -> Result<u64, std::io::Error> {
@@ -375,7 +423,7 @@ fn day_one_part_two(path: &str) -> Result<u64, std::io::Error> {
 #[cfg(test)]
 mod tests {
     use crate::{
-        day_3_part_1, day_3_part_2, day_4_part_1, day_one_part_one, day_one_part_two,
+        day_3_part_1, day_3_part_2, day_4_part_1, day_4_part_2, day_one_part_one, day_one_part_two,
         day_two_part_one, day_two_part_two,
     };
 
@@ -419,5 +467,11 @@ mod tests {
     fn day_4_part_1_example() {
         let result = day_4_part_1("./data/day_4_example.txt").unwrap();
         assert_eq!(result, 4512);
+    }
+
+    #[test]
+    fn day_4_part_2_example() {
+        let result = day_4_part_2("./data/day_4_example.txt").unwrap();
+        assert_eq!(result, 1924);
     }
 }
