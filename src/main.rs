283c0df869304fce23d653455030462b01eec4c2
Miguel Mendes <miguelmendeseiriz@gmail.com>
2021-12-15 22:13:32 +0000

    day 5 part 1.

--- src/main.rs
+++ src/main.rs
@@ -43,6 +43,84 @@ fn main() {
         "The last winning board final score would be: {}",
         day_4_part_2("./data/day_4_data.txt").unwrap()
     );
+
+    println!(
+        "There are {} points that at least two lines overlap",
+        day_5_part_1("./data/day_5_data.txt").unwrap()
+    );
+}
+
+fn day_5_part_1(path: &str) -> Result<u64, std::io::Error> {
+    let file = std::fs::read_to_string(path)?;
+    let lines: Vec<Vec<Vec<u32>>> = file
+        .split('\n')
+        .filter_map(|s| {
+            if s.is_empty() {
+                None
+            } else {
+                Some(
+                    s.split("->")
+                        .map(|points| {
+                            points
+                                .trim()
+                                .split(',')
+                                .filter_map(|coord| coord.parse().ok())
+                                .collect()
+                        })
+                        .collect(),
+                )
+            }
+        })
+        .collect();
+
+    let max_point = lines.iter().flatten().flatten().max().unwrap() + 1;
+
+    let mut overlap_grid = vec![vec![0u32; max_point as usize]; max_point as usize];
+
+    for line in lines {
+        let a = &line[0];
+        let b = &line[1];
+
+        let same_x = a[0] != b[0];
+        let same_y = a[1] != b[1];
+        let is_diagonal = same_x && same_y;
+        if !is_diagonal {
+            if same_x {
+                for x in a[0]..=b[0] {
+                    let y = a[1] as usize;
+                    overlap_grid[y][x as usize] += 1;
+                }
+
+                // TODO find a way to not repeat both ways
+                for x in b[0]..=a[0] {
+                    let y = a[1] as usize;
+                    overlap_grid[y][x as usize] += 1;
+                }
+            }
+
+            if same_y {
+                for y in a[1]..=b[1] {
+                    let x = a[0] as usize;
+                    overlap_grid[y as usize][x] += 1;
+                }
+
+                // TODO find a way to not repeat both ways
+                for y in b[1]..=a[1] {
+                    let x = a[0] as usize;
+                    overlap_grid[y as usize][x] += 1;
+                }
+            }
+        }
+    }
+
+    let total = overlap_grid.iter().flatten().fold(0u32, |mut sum, &value| {
+        if value >= 2 {
+            sum += 1
+        }
+        sum
+    });
+
+    Ok(total as u64)
 }
 
 fn day_4_part_2(path: &str) -> Result<u64, std::io::Error> {
@@ -423,8 +501,8 @@ fn day_one_part_two(path: &str) -> Result<u64, std::io::Error> {
 #[cfg(test)]
 mod tests {
     use crate::{
-        day_3_part_1, day_3_part_2, day_4_part_1, day_4_part_2, day_one_part_one, day_one_part_two,
-        day_two_part_one, day_two_part_two,
+        day_3_part_1, day_3_part_2, day_4_part_1, day_4_part_2, day_5_part_1, day_one_part_one,
+        day_one_part_two, day_two_part_one, day_two_part_two,
     };
 
     #[test]
@@ -473,5 +551,11 @@ mod tests {
     fn day_4_part_2_example() {
         let result = day_4_part_2("./data/day_4_example.txt").unwrap();
         assert_eq!(result, 1924);
+    }
+
+    #[test]
+    fn day_5_part_1_example() {
+        let result = day_5_part_1("./data/day_5_example.txt").unwrap();
+        assert_eq!(result, 5);
     }
 }
