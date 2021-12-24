2e2be7cb60bc92d34a2284776181b2b128b5588f
Miguel Mendes <miguelmendeseiriz@gmail.com>
2021-12-24 14:12:10 +0000

    day 7 part 2.

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -3,6 +3,11 @@ fn main() {
         "The least amount of fuel we can spend to align all crabs is: {}",
         align_crabs("data.txt").unwrap()
     );
+
+    println!(
+        "The least amount of fuel we can spend to align all crabs is: {}",
+        align_crabs_part_2("data.txt").unwrap()
+    );
 }
 
 fn align_crabs(path: &str) -> Result<i32, std::io::Error> {
@@ -27,20 +32,49 @@ fn align_crabs(path: &str) -> Result<i32, std::io::Error> {
     Ok(fuel_spent.into_iter().min().unwrap())
 }
 
+fn align_crabs_part_2(path: &str) -> Result<i32, std::io::Error> {
+    let file = std::fs::read_to_string(path)?;
+    let values: Vec<&str> = file.split('\n').filter(|s| !s.is_empty()).collect();
+    let initial_crabs: Vec<i32> = values[0]
+        .split(',')
+        .filter_map(|v| v.parse().ok())
+        .collect();
+
+    let fuel_spent: Vec<i32> = (0..initial_crabs.len())
+        .into_iter()
+        .map(|index| {
+            initial_crabs.iter().fold(0i32, |mut sum, &crab_offset| {
+                let position = index + 1;
+                let distance = ((position - crab_offset as usize) as i32).abs();
+                sum += ((distance * distance) + distance) / 2;
+                sum
+            })
+        })
+        .collect();
+
+    Ok(fuel_spent.into_iter().min().unwrap())
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::align_crabs;
+    use crate::{align_crabs, align_crabs_part_2};
 
     #[test]
     fn align_crabs_example() {
         let result = align_crabs("example.txt").unwrap();
         assert_eq!(result, 37);
     }
+
     #[test]
     fn align_crabs_attempt_1() {
-        // Answer was too low
         let result = align_crabs("data.txt").unwrap();
         assert_ne!(result, 25647);
         assert!(result > 25647);
+    }
+
+    #[test]
+    fn align_crabs_part_2_example() {
+        let result = align_crabs_part_2("example.txt").unwrap();
+        assert_eq!(result, 168);
     }
 }
