7d3f698e8c3f7dc856e4a0ff11e5465857e952e2
Miguel Mendes <miguelmendeseiriz@gmail.com>
2021-12-30 09:51:26 +0000

    day 11 part 2.

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -12,12 +12,19 @@ const NEXT: [(isize, isize); 8] = [
 const SIZE: usize = 10;
 
 fn main() {
-    let start = std::time::Instant::now();
+    let mut start = std::time::Instant::now();
     let steps = 100;
     println!(
         "The total amount of flashes after {} steps is: {} in {}s",
         steps,
         total_flashes("data.txt", steps).unwrap(),
+        start.elapsed().as_secs_f64()
+    );
+
+    start = std::time::Instant::now();
+    println!(
+        "The total amount of steps until a full flash is: {} in {}s",
+        steps_until_full_flash("data.txt").unwrap(),
         start.elapsed().as_secs_f64()
     );
 }
@@ -60,9 +67,32 @@ fn total_flashes(path: &str, steps: usize) -> Result<usize, std::io::Error> {
     Ok(sum)
 }
 
+fn steps_until_full_flash(path: &str) -> Result<usize, std::io::Error> {
+    let mut m: Vec<Vec<u8>> = std::fs::read(path)?
+        .lines()
+        .filter_map(std::result::Result::ok)
+        .map(|line| line.split("").filter_map(|c| c.parse().ok()).collect())
+        .collect();
+
+    let sum = (1..usize::MAX)
+        .find(|_step| {
+            m.iter_mut()
+                .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
+            (0..SIZE)
+                .flat_map(|y| (0..SIZE).map(move |x| (x, y)))
+                .fold(0, |acc, (x, y)| {
+                    acc + (m[y][x] > 9).then(|| propagate(&mut m, x, y)).unwrap_or(0)
+                })
+                == SIZE * SIZE
+        })
+        .unwrap();
+
+    Ok(sum)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::total_flashes;
+    use crate::{steps_until_full_flash, total_flashes};
 
     #[test]
     fn total_flashes_example() {
@@ -70,5 +100,11 @@ mod tests {
         assert_eq!(result, 204);
         result = total_flashes("example.txt", 100).unwrap();
         assert_eq!(result, 1656);
+    }
+
+    #[test]
+    fn steps_until_full_flash_example() {
+        let result = steps_until_full_flash("example.txt").unwrap();
+        assert_eq!(result, 195);
     }
 }
